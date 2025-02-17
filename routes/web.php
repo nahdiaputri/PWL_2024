b1eafe185b26e184cebcd7a60ec0ad93938bd8f2
Nahdia Putri <nptrsfr1808@gmail.com>
2025-02-17 10:00:25 +0700

    awal

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,36 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+Route::get('/hello', function() {
+    return 'Hello World';
+
+});
+
+Route::get('/', function() {
+    return 'Selamat Datang';
+
+});
+
+Route::get('/about', function () {
+    return 'NIM: 2341720015 - Nama: Nahdia Putri Safira';
+
+});
+
+Route::get('/user/{name}', function ($name) {
+    return 'Nama saya ' . $name;
+});
+
+Route::get('/posts/{post}/comments/{comment}', function($postId, $commentId) {
+    return 'Pos ke-'. $postId." Komentar ke-: ".$commentId;
+});
+Route::get('/articles/{id}', function ($id) {
+    return 'Halaman Artikel dengan ID ' . $id;
+});
+
+Route::get('/user/{name?}', function ($name=null){
+    return 'Nama saya '.$name;
+});
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +43,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
